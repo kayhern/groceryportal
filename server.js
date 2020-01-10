ead6fa1c3a2112fbb285b2c3fb4d06946c324e41
Lisa <lisa.michael@hotmail.com>
2020-01-09 21:05:58 -0500

    latest express server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// *********************************************************************************
+// *****************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
-// *********************************************************************************
-
-// Dependencies
+//
+// ******************************************************************************
+// *** Dependencies
 // =============================================================
 var express = require("express");
 
@@ -11,20 +11,29 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
+var db = require("./models");
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("app/public"));
+app.use(express.static("public"));
 
 // Routes
 // =============================================================
-// require("./app/routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+// HTML routes
+require("./routes/html-routes")(app);
 
-// Starts the server to begin listening
+// API routes 
+require("./routes/api-routes")(app);
+
+
+// Syncing our sequelize models and then starting our Express app
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
